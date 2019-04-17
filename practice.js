8931e3dcbda04c385b96df29eacf2d37ee8b66e6
mnorty <mnorty@gmail.com>
2019-04-17 16:25:39 -0600

    First 2 answered

--- practice.js
+++ practice.js
@@ -27,7 +27,9 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here 
+function first(thing,cb){
+  cb(thing[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -47,7 +49,10 @@ first(names, function(firstName){
   Then invoke the callback, passing in the last element in the array as the argument.
 */
 
-//Code Here
+function last(arr,cb){
+  i = arr.length - 1
+  cb(arr[i])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
